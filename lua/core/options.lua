1df1001e433dd568c9e466cd6e888daef3f114bb
Felix Schlenkrich <felix.schlenkrich@gmail.com>
2025-07-31 19:49:31 +0200

    init commit

---
+++ lua/core/options.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.mouse = 'a'
+vim.o.showmode = true
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true -- Enable break indent
+
+vim.o.undofile = true -- Save undo history
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = 'yes' -- Keep signcolumn on by default
+
+vim.o.updatetime = 250 -- Decrease update time
+vim.o.timeoutlen = 300 -- Decrease mapped sequence wait time
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.inccommand = 'split' -- Preview substitutions live, as you type!
+
+vim.o.cursorline = true -- Show which line your cursor is on
+
+vim.o.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+vim.o.confirm = true
+
