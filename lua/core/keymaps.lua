06a05190aeec91a144a3eda75635b06f1abe63b9
Felix Schlenkrich <felix.schlenkrich@gmail.com>
2025-08-03 22:28:04 +0200

    add copilot.chat and obsidian

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,12 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>od', '<cmd>ObsidianToday<cr>', { desc = '[O]bsidian [D]aily Note' })
+vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<cr>', { desc = '[O]bsidian [N]ew Note' })
+vim.keymap.set('n', '<leader>os', '<cmd>ObsidianSearch<cr>', { desc = '[O]bsidian [S]earch Note' })
+vim.keymap.set('n', '<leader>of', '<cmd>ObsidianFollowLink<cr>', { desc = '[O]bsidian [F]ollow Link' })
+vim.keymap.set('n', '<leader>ob', '<cmd>ObsidianBacklinks<cr>', { desc = '[O]bsidian [B]acklinks' })
+vim.keymap.set('n', '<leader>oo', '<cmd>ObsidianOpen<cr>', { desc = '[O]bsidian [O]pen in Obsidian' })
+
+
