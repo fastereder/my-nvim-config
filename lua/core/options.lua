bc252786009a642042291b3ac83e5b6784e0b11a
Felix Schlenkrich <felix.schlenkrich@gmail.com>
2025-08-03 11:55:48 +0200

    add copilot.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -45,4 +45,3 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "BufReadPost",
+  opts = {
+    suggestion = {
+      enabled = false,
+      auto_trigger = false,
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
